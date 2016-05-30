a0ca87ba9d58784cdf1cab6c7dc5ebf73c980516
wengshengyuan <wengshengyuan@gmail.com>
2016-05-30 19:12:38 +0800

    #

--- src/main/java/tcp2httpbridge/tcpendpoint/TCPServer.java
+++ src/main/java/tcp2httpbridge/tcpendpoint/TCPServer.java
@@ -41,9 +41,7 @@ public class TCPServer extends Thread{
 			} else {
 				logger.info("HTTP返回:"+result);
 				byte[] en = result.getContent().getBytes();
-				logger.info("byte[]="+en);
 				byte[] de = Base64Util.decryBytes(en);
-				logger.info("de byte[]="+de);
 				logger.info("TCP 写入:"+new String(de));
 				OutputStream os = socket.getOutputStream();
 				os.write(de);
